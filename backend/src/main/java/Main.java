c1f33141f5d883ce1e3d68adcb55c33ad212b99a
xSilverline <szkarna@gmail.com>
2026-04-14 05:24:00 +0200

    Test

--- backend/src/main/java/Main.java
+++ backend/src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-  public static void main() {
-    System.out.println("Hello, World!");
-  }
+    public static void main() {
+
+
+        System.out.println("Hello, World!");
+
+
+    }
 }
