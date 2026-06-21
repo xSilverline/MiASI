f535f1721459a04ff6cf6e44471d63baa3477600
Autimus <akcja53@gmail.com>
2026-06-21 20:09:04 +0200

    Not working hexagonal tests

--- backend/src/test/java/miasi/backend/architecture/PackageArchTests.java
+++ backend/src/test/java/miasi/backend/architecture/PackageArchTests.java
@@ -6,13 +6,59 @@ import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 @AnalyzeClasses(packages = "miasi.backend", importOptions = ImportOption.DoNotIncludeTests.class)
 public class PackageArchTests {
 
   @ArchTest()
-  static final ArchRule enumPackage = classes()
+  static final ArchRule enums_should_be_in_enums_package = classes()
       .that().areEnums()
       .should().resideInAPackage("..enums..");
+
+  @ArchTest
+  static final ArchRule events_should_be_in_events_package =
+      classes()
+          .that()
+          .areAssignableTo(java.util.EventObject.class)
+          .should()
+          .resideInAnyPackage("..events..");
+
+  @ArchTest
+  static final ArchRule repositories_should_be_only_in_ports_or_database =
+      classes()
+          .that().areNotInterfaces()
+          .and().haveSimpleNameContaining("Repository")
+          .should()
+          .resideInAnyPackage(
+              "..database..",
+              "..ports.."
+          );
+
+  @ArchTest
+  static final ArchRule controllers_should_be_in_api =
+      classes()
+          .that()
+          .haveSimpleNameContaining("Controller")
+          .should()
+          .resideInAnyPackage("..api..");
+
+  @ArchTest
+  static final ArchRule services_should_not_be_in_domain =
+      noClasses()
+          .that()
+          .haveSimpleNameEndingWith("Service")
+          .should()
+          .resideInAnyPackage("..domains..");
+
+  @ArchTest
+  static final ArchRule dto_should_be_in_api =
+      classes()
+          .that()
+          .haveSimpleNameContaining("Request")
+          .or()
+          .haveSimpleNameContaining("Response")
+          .should()
+          .resideInAnyPackage("..api.jsons..");
 }
 
